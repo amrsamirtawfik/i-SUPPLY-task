9ae5b5d5fe1f6b7278238fce7da3acce11f9a4a1
amrsamirtawfik <amrsamirtawfik@gmail.com>
2023-10-24 07:04:06 +0300

    updating loginscreen

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -12,6 +12,7 @@ class _LoginScreen extends State<LoginScreen> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   bool isPasswordVisible = false;
+  bool loginError = false;
 
   void togglePasswordVisibility() {
     setState(() {
@@ -22,9 +23,9 @@ class _LoginScreen extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Screen'),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Login Screen'),
+      // ),
       body: StreamBuilder<Object>(
           stream: authenticationBloc.authenticationStream,
           builder: (context, snapshot) {
@@ -32,95 +33,104 @@ class _LoginScreen extends State<LoginScreen> {
             if (snapshot.hasData) {
               if (snapshot.data is AuthenticatedState) {
                 // Navigate to the dashboard screen on successful login
+                loginError = false;
                 print('authnicated ya zmeeeellll');
               } else if (snapshot.data is UnauthenticatedState) {
                 // Show an error message on unsuccessful login
+                loginError = true;
               }
             }
-            return Center(
-              child: SizedBox(
-                width: 1100,
-                child: Card(
-                  margin: const EdgeInsets.all(100),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(100)), // Set the borderRadius to null
-                  ),
-                  elevation: 4,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Image.asset('assets/images/medicine.jpg'),
+            return Container(
+              padding: const EdgeInsets.all(100),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    // child: Image.asset('assets/images/medicine.jpg'),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(100),
+                        bottomLeft: Radius.circular(100),
                       ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          padding: const EdgeInsets.all(50),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                'Welcome to i\'SUPPLY pharmacy!'
-                                '\n'
-                                '\n'
-                                'Please Login',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
-                              ),
-                              TextFormField(
-                                decoration: const InputDecoration(
-                                    labelText: 'Username'),
-                                controller: usernameController,
-                              ),
-                              const SizedBox(height: 20),
-                              TextFormField(
-                                controller: passwordController,
-                                decoration: InputDecoration(
-                                  labelText: 'Password',
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      isPasswordVisible
-                                          ? Icons.visibility
-                                          : Icons.visibility_off,
-                                    ),
-                                    onPressed: togglePasswordVisibility,
-                                  ),
-                                ),
-                                obscureText:
-                                    !isPasswordVisible, // Toggle the obscureText property
-                              ),
-                              const SizedBox(height: 20),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Implement sign-in logic here
-                                  authenticationBloc.login(
-                                      usernameController.text,
-                                      passwordController.text);
-                                },
-                                child: const Text('Sign In'),
-                              ),
-                              const SizedBox(height: 20),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Implement sign-up logic here
-                                },
-                                child: const Text('Sign Up'),
-                              ),
-                              const SizedBox(height: 20),
-                              TextButton(
-                                onPressed: () {
-                                  // Implement forgot password logic here
-                                },
-                                child: const Text('Forgot Password?'),
-                              ),
-                            ],
+                      child: Image.asset(
+                        'assets/images/medicine.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      width: 400,
+                      padding: const EdgeInsets.all(50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Welcome to i\'SUPPLY pharmacy!'
+                            '\n'
+                            '\n'
+                            'Please Login',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
                           ),
-                        ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Username'),
+                            controller: usernameController,
+                          ),
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: togglePasswordVisibility,
+                              ),
+                            ),
+                            obscureText:
+                                !isPasswordVisible, // Toggle the obscureText property
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Implement sign-in logic here
+                              authenticationBloc.login(usernameController.text,
+                                  passwordController.text);
+                            },
+                            child: const Text('Sign In'),
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Implement sign-up logic here
+                            },
+                            child: const Text('Sign Up'),
+                          ),
+                          const SizedBox(height: 20),
+                          TextButton(
+                            onPressed: () {
+                              // Implement forgot password logic here
+                            },
+                            child: const Text('Forgot Password?'),
+                          ),
+                          loginError
+                              ? const Text(
+                                  'Username or password is incorrect!',
+                                  style: TextStyle(color: Colors.red),
+                                )
+                              : const SizedBox()
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
             );
           }),
